419e5d3a42326d6b6fe53de006276b5f106f9b6c
SujuPatil <SujayKumar.Dgowder@thomsonreuters.com>
2025-02-27 20:53:48 +0530

    v1

---
+++ 01_Fundamentals/src/com/dl/Fundamentals/DataTypes.java
@@ -0,0 +1,22 @@
+package com.dl.Fundamentals;
+
+public class DataTypes {
+	
+	public static void main(String[] args) {
+		
+		//example like an purchasing grocery
+		
+		int product_id = 2345;
+		String product_name = "Eggs";
+		byte produt_quantity = 6;
+		float product_price = 20.00f;
+		boolean product_available = true;
+		
+		String user_name = "suresh";
+		Long contact_number = 9900442211L;
+		char user_gender = 'M';
+		String user_address = "Bangalore";
+		
+	}
+
+}
---
+++ 01_Fundamentals/src/com/dl/Fundamentals/Test.java
@@ -0,0 +1,16 @@
+package com.dl.Fundamentals;
+
+//class name
+public class Test {
+
+	//Main Method
+	public static void main(String[] args) {
+
+		//Statement
+		System.out.println("Hello World");
+	}
+
+}
+
+
+//
